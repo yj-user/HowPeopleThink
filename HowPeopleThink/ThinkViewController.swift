91b294dbb222a358db84f3c7a9342028f22d9d8d
yj.user <apffhs41@naver.com>
2021-05-09 19:56:38 +0900

    SwiftyJSON 으로 트윗 내용 끌어오기, ML Model Prediction 으로 트윗 Label 받아와서 값에 따른 점수화 후 점수 범위에 따라 이모티콘 구현

--- HowPeopleThink/ThinkViewController.swift
+++ HowPeopleThink/ThinkViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreML
 import SwifteriOS
+import SwiftyJSON
 
 class ThinkViewController: UIViewController {
 
@@ -22,16 +23,69 @@ class ThinkViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swifter.searchTweet(using: "@apple", lang: "en", count: 100, tweetMode: .extended) { (results, error) in
-            print(results)
-        } failure: { (Error) in
-            print("Tweet search failed: \(Error)")
-        }
-
-        
     }
 
     @IBAction func predictButtonPressed(_ sender: UIButton) {
+        if let object = myTextField.text {
+            
+            swifter.searchTweet(using: object, lang: "en", count: 100, tweetMode: .extended) { (results, error) in
+                
+                var peopleThinkArray = [HowPeopleThinkInput]()
+                
+                for i in 0..<100 {
+                    if let text = results[i]["full_text"].string {
+                        
+                        let peopleThinkText = HowPeopleThinkInput(text: text)
+                        peopleThinkArray.append(peopleThinkText)
+                    }
+                }
+                
+                do {
+                    let prediction = try self.peopleThinkModel.predictions(inputs: peopleThinkArray)
+                    
+                    var score = 0
+                    
+                    for pred in prediction {
+                        if pred.label == "Pos" {
+                            score += 1
+                        } else if pred.label == "Neg" {
+                            score -= 1
+                        }
+                    }
+                    
+                    if score > 20 {
+                        self.thinkEmoji.text = "😍 \(score)"
+                        self.thinkEmoji.textColor = UIColor.green
+                    } else if score > 10 {
+                        self.thinkEmoji.text = "😄 \(score)"
+                        self.thinkEmoji.textColor = UIColor.green
+                    } else if score > 0 {
+                        self.thinkEmoji.text = "☺️ \(score)"
+                        self.thinkEmoji.textColor = UIColor.black
+                    } else if score == 0 {
+                        self.thinkEmoji.text = "😐 \(score)"
+                        self.thinkEmoji.textColor = UIColor.black
+                    } else if score > -10 {
+                        self.thinkEmoji.text = "😕 \(score)"
+                        self.thinkEmoji.textColor = UIColor.black
+                    } else if score > -20 {
+                        self.thinkEmoji.text = "😡 \(score)"
+                        self.thinkEmoji.textColor = UIColor.red
+                    } else {
+                        self.thinkEmoji.text = "🤮 \(score)"
+                        self.thinkEmoji.textColor = UIColor.red
+                    }
+                    
+                } catch {
+                    print("Fail to fetch Tweet: \(error)")
+                }
+                
+                
+                
+            } failure: { (Error) in
+                print("Tweet search failed: \(Error)")
+            }
+        }
     }
     
 }
